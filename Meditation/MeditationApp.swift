df3c5f19de8d9bce928b5347e5bc1d9280bd9b8b
Jonas Agnezi <jonas.agnezi@gmail.com>
2022-07-12 21:36:20 -0300

    Initial Commit

---
+++ Meditation/MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationApp.swift
+//  Meditation
+//
+//  Created by Jonas Agnezi on 12/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
